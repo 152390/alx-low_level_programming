09cc498e348d66087e5b559daeafb5bcd1859bc8
152390 <giftmorris198@gmail.com>
2022-12-15 01:27:54 -0800

    Task 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,21 +6,19 @@
  */
 
 int main(void)
-
 {
-	int i;
+int i;
 
-	printf("infinite loop incoming :(\n");
+printf("infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 /*
- * 	while (i < 10)
- * 	{
- * 		putchar(i);
- * 	}
+ *while (i < 10)
+ *{
+ *putchar(i);
+ *}
  */
+printf("infinite loop avoided! \\o/\n");
 
-	printf("infinite loop avoided! \\o/\n");
-
-	return (0);
+return (0);
 }
