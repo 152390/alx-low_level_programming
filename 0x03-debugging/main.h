7bf237934a75f9bd2a49ab26a8882af7b6fca5c1
152390 <giftmorris198@gmail.com>
2022-12-16 01:12:54 -0800

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,9 +5,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-void print_remainning_days(int month, int day, int year);
 int convert_day(int month, int day);
-
+void print_remainning_days(int month, int day, int year); 
 
 #endif /* MAIN_H */
 
