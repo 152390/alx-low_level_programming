fb72fb12f74f5a87ad9daeae6e86ddf308b6a8c7
152390 <giftmorris198@gmail.com>
2022-12-16 07:30:58 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,11 +10,11 @@ void more_numbers(void)
 
 int i, ro;
 
-for (ro = 0; ro < 10; ro++)
+for (ro = 0; ro <= 10; ro++)
 {
 for (i = 0; i <= 14; i++)
 {
-if (i >= 10)
+if (i > 9)
 
 _putchar((i / 10) + '0');
 
