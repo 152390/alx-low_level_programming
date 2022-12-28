d28412fdabe7fb5e2df5bcd0e6595c4d588d2c4f
152390 <giftmorris198@gmail.com>
2022-12-28 12:28:56 -0800

    Task 9

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,10 +2,10 @@
 
 /**
  * leet - Encodes a string to 1337.
- * @str: The string to be encoded.
- *
- * Return: A pointer to the encoded string.
- */
+* @str: The string to be encoded.
+*
+* Return: A pointer to the encoded string.
+*/
 char *leet(char *str)
 {
 	int indx1 = 0, indx2;
@@ -13,18 +13,20 @@ char *leet(char *str)
 
 	while (str[indx1])
 	{
-
 		for (indx2 = 0; indx2 <= 7; indx2++)
 		{
-
 			if (str[indx1] == leet[indx2] ||
 			    str[indx1] - 32 == leet[indx2])
 				str[indx1] = indx2 + '0';
-		}
+		
 
-		indx1++;
-	}
 
-	return (str);
-}
-Footer
+
+
+
+
+
+
+
+
+
