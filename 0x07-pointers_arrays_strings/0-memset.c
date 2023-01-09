7e282f6b06e4bda8360dc736a199654caebd650a
152390 <giftmorris198@gmail.com>
2023-01-09 14:35:18 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* simple_print_butter - printer buffer in hexa
+* @buffer: the address of memory to print
+* @size: the size of the memory to print
+*
+* Return: Nothing.
+*/
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+unsigned int i
+ i = 0;
+while (i < size)
+{
+if (i 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
