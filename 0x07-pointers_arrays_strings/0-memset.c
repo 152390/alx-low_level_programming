d0c72cf08ac2b4347d2fa475710e9362fced192e
152390 <giftmorris198@gmail.com>
2023-01-10 07:53:11 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,21 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * *_memset - Fills memory with a constant byte
- *
- * @s: pointer
- * @b: char
- * @n: unsigned int* @n: unsigned int
- *
- *Return: s
- */
+* _memset - fills memory with a constant byte
+* @s: source string
+* @b: the content byte for filling
+* @n: lenght of buffer
+* Return: new string.
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
+	unsigned int i = 0;
 
-	for (index = 0; n > 0; index++)
+	while (i < n)
 	{
-		s[index] = b;
-		n -= 1;
+		*(s + i ) =b;
+		i++;
 	}
 	return (s);
 }
+
