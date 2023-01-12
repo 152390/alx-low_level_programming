772f7b6824453c6b7884b7d29b30d8311fb868e4
152390 <giftmorris198@gmail.com>
2023-01-12 00:13:16 -0800

    Task 0

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  File: main.h
+ *  Auth: Firdaus
+ *  Desc: Header file containing prototypes for all functions
+ *   	  used in the 0x07-pointers_arrays_strings directory.
+ */
+
+#include <stddef.h>
+
+
 int _putchar(char c)
 char *_memset(char *s, char b, unsigned int n);
 char *-memcpy(char *dest, char *src, unsigned int n);
