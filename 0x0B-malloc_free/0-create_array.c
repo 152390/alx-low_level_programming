1336eabaa34ccdb2c09a8490c7ef3a2ebba93da2
152390 <giftmorris198@gmail.com>
2023-01-12 04:50:57 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
 	for (index = 0; index < size; index++)
 		array[index] = c;
 
