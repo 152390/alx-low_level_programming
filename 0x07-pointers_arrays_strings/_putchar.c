71ddb68507cfb334680ed2b2ac690fc01eeca1ad
152390 <giftmorris198@gmail.com>
2023-01-12 01:17:13 -0800

    Task 0

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <unsitd.h>
 
 /**
  * _putchar - writes the character c to stdout
  * @c: the character to print
+ *
  * Return on success 1.
  * on error, -1 is returned, and error is set approperately.
  */
@@ -11,3 +11,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,17 +10,17 @@
 
 #include <stddef.h>
 
-
 int _putchar(char c)
-char *_memset(char *s, char b, unsigned int n);
-char *-memcpy(char *dest, char *src, unsigned int n);
+void *_memset(void *s, int c, size_t n);
+void *_memcpy(void *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char _*strpbrk(char *s, char accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif
+
 
