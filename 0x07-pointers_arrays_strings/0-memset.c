5b8ffd35c933237525998fe73f1ad656adcef635
152390 <giftmorris198@gmail.com>
2023-01-13 00:25:31 -0800

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,11 +6,11 @@
  * @b: constant
  * @n: max bytes to use
  * Return: s
- */
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	signed int i;
 
 	for (i = 0; n > 0; i++, n--)
 	{
