6054a8e90ebc5a1ff8e7e767e4fa5ecf57d27d9f
152390 <giftmorris198@gmail.com>
2023-01-13 00:20:17 -0800

    Task 0

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -2,10 +2,10 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: The character to print
  *
- * Return on success 1.
- * on error, -1 is returned, and error is set approperately.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
