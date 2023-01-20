9c1e31bfca80254c632f8174eaa08c785b42fe79
152390 <giftmorris198@gmail.com>
2023-01-20 05:46:01 -0800

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,6 +30,12 @@ void print_all(const char * const format, ...)
 		case 'c':
 			printf("%c", va_arg(valist, int)), c = 1;
 			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;	
 		case 's':
 			str = va_arg(valist, char *), c = 1;
 			if (!str)
