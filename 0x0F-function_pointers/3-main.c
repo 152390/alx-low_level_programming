411aa9faa389e47ff2bc1567259e70471f74975a
152390 <giftmorris198@gmail.com>
2023-01-23 01:44:03 -0800

    Task 0

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,42 +1,39 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "3-calc.h"
 
 /**
- * main - Prints result of an operation
- * @argc: Number of arguments
- * @argv: Value of arguments
- * Return: Returns an int.
+ * main - performs simple operations
+ * @argc: Number of arguments passed
+ * @argv: arrry of pointer arguments
+ *
+ * Return: always 0
  **/
 
 int main(int argc, char *argv[])
 {
+	int a, b, c;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = f(a, b);
+	printf("%d\n", c);
+	return (0);
+}
+
 if (argc != 4)
 exit(98);
-}
-
-if (
-(strcmp(argv[2], "+") &&
-strcmp(argv[2], "-") &&
-strcmp(argv[2], "*") &&
-strcmp(argv[2], "/") &&
-strcmp(argv[2], "%"))
-)
-{
-printf("Error\n");
-exit(99);
-}
-
-if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-&& atoi(argv[3]) == 0)
-{
-printf("Error\n");
-exit(100);
-}
-
-printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
-
-return (0);
 }
 
