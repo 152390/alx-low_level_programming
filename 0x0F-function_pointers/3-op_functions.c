e5ce006b4e9eb0fe7e21aa590bc5b03003cd5931
152390 <giftmorris198@gmail.com>
2023-01-23 02:13:11 -0800

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,69 +1,70 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * op_add - Adds two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
- **/
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - subtracts two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
- **/
+ * op_sub - subtracts b from a
+ * @a: first number
+ *
+ * Return: difference of a and b
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
-}
-
 /**
- * op_mul - Multiplies two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
- **/
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * op_mul - Multiplies two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
- **/
-
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
+ */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
-
 /**
- * op_div - Divides two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
- **/
-
+ * op_div - divides a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: integer division of a by b
+ */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - Modules two numbers
- * @a: int 1
- * @b: int 2
- * Return: Returns result
- **/
-
+ * op_mod - find the remainder of the division of a by b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: remainder of the division of a by b
+ */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
 
