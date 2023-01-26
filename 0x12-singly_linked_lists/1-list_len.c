1d6ddc021bd68105abedd1d58f78715728f89fca
152390 <giftmorris198@gmail.com>
2023-01-26 00:20:25 -0800

    Task 2

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * list_len - Calculate the number of elements
+ * @h: Pointer to a list
+ * Return: Integer
+ **/
+size_t list_len(const list_t *h)
+{
+	size_t n_nodes = 0;
+	if (!h)
+		return (0);
+	while (h)
+	{
+		n_nodes++;
+		h = h->next;
+	}
+	return (n_nodes);
+}
+
