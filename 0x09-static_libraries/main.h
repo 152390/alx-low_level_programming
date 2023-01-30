d70e9476b239e5ffbb09b6602ac3b663d7073ce0
152390 <giftmorris198@gmail.com>
2023-01-30 05:46:59 -0800

    Task 1

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-/*
+/**
  * File: main.h
- * Auth: Firdaus
- * Desc: Header file containing the prototypes of all functions
- * 	 included in the static library libholberton.a.
+ * 
+ * Desc: header file
  */
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
