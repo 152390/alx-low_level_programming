bfecb08faf8a164695dff2de1da2c0377adfb3ad
152390 <giftmorris198@gmail.com>
2023-02-02 05:13:21 -0800

    Task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-print_binary.c
+ * Auth: Gift Morris Abia
+ */
+
+#include "holberton.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
+
